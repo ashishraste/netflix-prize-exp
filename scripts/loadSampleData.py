c43a2bd43e6fea8a3cb0781418487ff310ac4566
Ashish Raste <rasteashish@gmail.com>
2013-11-08 11:30:06 +0800

    sorted userIDs in probe set

--- scripts/loadSampleData.py
+++ scripts/loadSampleData.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+from tempfile import mkdtemp
+import os.path as path
 import os as os
 import numpy as np
 import random
@@ -7,82 +8,61 @@ import time
 DATASET_SIZE = 17770
 SAMPLE_SIZE = DATASET_SIZE / 10
 
-DIR_PATH = "/media/01244D4977874BCC/training_set/"
-# get the files name in the directory DIR
+DIR_PATH = "/Users/OrangeR/Documents/study/AY2013_14/CS5228/group project/Dataset of Netflix/training_set/"
+#get the files name in the directory DIR
+
+# write into the memmap array (which creates a virtual file through mkdtemp)
+# the values of movieID and their average rating
+#virtualFilename = path.join(mkdtemp(), 'ratingsfile.dat')
+#fp = np.memmap(virtualFilename, dtype='int32', mode='w+', shape=(SAMPLE_SIZE,1))
 
 # pick a random sample of 1/10th the size of the dataset
-randomNumList = []
+randomNumList=[]
 sampleFilesList = []
 for i in range(SAMPLE_SIZE):
     randomNumList.append(random.randrange(1, DATASET_SIZE ,1))
-
-os.chdir(DIR_PATH)    
+    
 past = time.time()
 
 # collect all the file names within the dataset
 files_array = []
+os.chdir(DIR_PATH)
 for files in os.listdir("."):
-    if files.endswith(".txt"):
+    if files.endswith(".txt") :
       files_array.append(files)
+      print files
 
 # create a dictionary to store all the filenames in the dataset indexed on their movieIDs
-movieIDDict = {}
-movieIDNameDict = {}
+movieIdDict = {}
 for fileName in files_array:    
-    if "mv_" in fileName:
         fileNum = fileName.split('mv_')
         fileNumList = fileNum[1].split('.')        
         fileNum = int(fileNumList[0])
-        movieIDDict[fileNum] = fileName
+        movieIdDict[fileNum] = fileName
 
 
 # open a movie file, calculate its average ratings 
 # store the average ratings in a outputfile, do this for all the files in the dataset
 averageRatingDict = {}
-numOfAllRatings = 0
-allRatingsAverage = 0
 def processMovieFiles(sampleFileName):
     #sampleFileNumList = sampleFileName.split('.')
     #sampleFileNum = int(sampleFileNumList[0])
-    #sampleFileName = movieIDDict[sampleFileNum]            
-    global allRatingsAverage
-    global numOfAllRatings
+    #sampleFileName = movieIdDict[sampleFileNum]            
     with open(sampleFileName) as f: 
         avgRating = 0       
         rows = f.readlines()
         del rows[0]
-        lineContentList = []
-        for lines in rows:                      
+        for lines in rows:          
+            lineContentList = []
             lineContentList = lines.split(',')                
-            avgRating += float(lineContentList[1])
-            allRatingsAverage += float(lineContentList[1])
-            numOfAllRatings += 1
-            del lineContentList[:]
+            avgRating = avgRating + float(lineContentList[1])
         avgRating = float(avgRating / len(rows))
+        lineContentList[:] = []
         #print f.name, avgRating    
         averageRatingDict[f.name] = avgRating
     f.close()    
     return (f.name, avgRating)    
     
-# writes average rating of each movie into avgRatingsOut.txt, calculates average of averages
-globalMeanRating = 0  # represents the mean of the mean ratings
-with open(DIR_PATH + "avgRatingsOut.txt", "a") as outfile:
-    for files in files_array:        
-        filename, avgRating = processMovieFiles(files)
-        #print filename, avgRating
-        globalMeanRating = globalMeanRating + avgRating
-        outfile.write(str(avgRating))
-    globalMeanRating /= DATASET_SIZE
-    print "GlobalMeanRating: ", globalMeanRating
-    # prints the average of all the ratings that were rated in the dataset
-    allRatingsAverage /= numOfAllRatings
-    print "allRatingsAverage: ", allRatingsAverage
-    print "total number of ratings: ", numOfAllRatings
-    outfile.write(str(globalMeanRating))
-    outfile.close()    
-        
-print "Time:%.3f" % (time.time() - past), "seconds"
-
 # create the filenames under the sample set using randomNumList
 # and calculate their averageRatings
 #fileType = '.txt'
@@ -90,10 +70,16 @@ print "Time:%.3f" % (time.time() - past), "seconds"
 #    sampleFileName = '{0}{1}'.format(randomNum, fileType) 
 #    #print sampleFileName   
 #    processMovieFiles(sampleFileName)
-
-def main():
     
 
+with open("/Users/OrangeR/Documents/cs5228/" + "avgRatingsOut.txt", "a") as outfile:
+    for files in files_array:        
+        filename, avgRating = processMovieFiles(files)
+        print filename, avgRating
+        outfile.write(str(avgRating))
+    outfile.close()
+    
+print "Time:%.3f" % (time.time() - past), "seconds"
 
 
 
--- scripts/ratingsCollection.py
+++ scripts/ratingsCollection.py
@@ -1,36 +1,19 @@
 import os as os
 import numpy as np
-import random
 import time
-from ctypes import *
-
-#class UserID(Structure):
-#    _fields_ = [("uid", c_ushort), ("rating", c_ubyte)]
-#
-#class MovieID(Structure):
-#    __fields__ = [("mid", c_ushort), ("rating", c_ubyte)]
 
 DATASET_SIZE = 17770
 SAMPLE_SIZE = DATASET_SIZE / 10
 
 DIR_PATH = "/media/01244D4977874BCC/training_set/"
 os.chdir(DIR_PATH)
-
-# pick a random sample of 1/10th the size of the dataset
-randomNumList = []
-sampleFilesList = []
-def generateRandomNumbers():
-    for i in range(SAMPLE_SIZE):
-        randomNumList.append(random.randrange(1, DATASET_SIZE ,1))
-
         
 # collect all the file names within the dataset
 files_array = []
-def collectFileNames():
+def getFileNames():
     for files in os.listdir("."):
         if files.endswith(".txt") and "mv_" in files:
             files_array.append(files)
-
 
 
 rByMIDDict = {}
@@ -84,7 +67,7 @@ def createUIDDict(sampleFileName):
             
 
 def main():
-    collectFileNames()
+    getFileNames()
     for movieFile in files_array:
         createMIDDict(movieFile)
 
